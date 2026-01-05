f900efc780beaa26a0a59137d383886216e8fac2
niyatijain912 <niyatijain912@gmail.com>
2026-01-05 21:19:33 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 /* ================= CONFIGURATION ================= */
 
 // 👇 PASTE YOUR RENDER URL HERE (No trailing slash)
-const API_BASE_URL = "https://nagrik-raskshak.onrender.com";
+const API_BASE_URL = "https://nagrik-raskshak1.onrender.com";
 
 // Use this one if you are testing locally on your laptop:
 // const API_BASE_URL = "http://localhost:3000"; 
@@ -123,3 +123,4 @@ async function login() {
     alert("Failed to connect to server. Please try again.");
   }
 }
+
